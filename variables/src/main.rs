ccd612e7737e40463105c90933713ba8e3290cbb
Adrian Lawson <adrianglawson@gmail.com>
2022-08-09 10:03:06 -0700

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,9 @@
+fn sum(a: i32, b:i32) -> i32 {
+    println!("first statement");
+    println!("second statement");
+    a + b
+}
+
 fn main() {
     let mut x = 2; // must include 'mut' in declara&tion to be able to edit it later
     let y = 5; // similar to a constant, not changeable 
@@ -108,12 +114,95 @@ fn main() {
     let location: String = "Sliema, Malta".to_string();
     let title: String = String::from("IT Engineer");
 
-    let title_slice = &title;
+    let title_slice = &title[0..6];
 
     println!("Hi, my name is {}.", name);
     println!("I live in {}.", location);
     println!("This name is {} characters long.", name.len());
     println!("My title is {}.", title);
     println!("Title as a string slice: {}.", title_slice);
+
+
+    let name = "Zsolt";
+    let name_len = name.len();
+
+    if name_len % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
+
+    if name_len ==4 {
+        println!("Four");
+    } else if name_len ==5 {
+        println!("Five");
+    } else if name_len == 6 {
+        println!("Six");
+    } else {
+        println!("Different");
+    }
+
+    let first = false;
+    let second = true;
+
+    if first && second {
+        println!("Both are true");
+    } else if first || second {
+        println!("At least one is true");
+    } else {
+        println!("Neither is true");
+    }
+
+    match name_len {
+        4 => {println!("Four");}
+        5 => {println!("Five");}
+        6 => {println!("Six");}
+        _ => {println!("Different");}
+    }
+
+    match first {
+        true => {println!("True");}
+        false => {println!("False");}
+    }
+
+    let message3 = match name_len {
+        4 => { "Four" }
+        5 => {"Five"}
+        6 => {"Six"}
+        _ => {"Different"}
+    };
+
+    println!("{}", message3);
+
+    let n = 10; 
+    let mut i = 1;
+
+    loop {
+        println!("loop: {}", i);
+        i = i + 1;
+        if i > n {
+            break;
+        }
+    }
+
+    println!("in-between loops: {}", i);
+    i = 1;
+
+    while !(i >= n) {
+        println!("While: {}", i);
+        i = i + 1;
+    }
+
+    for i in 1..=10 {
+        println!("for: {}", i);
+    }
+
+    for value in cheat_code {
+        println!("cheat code by value: {}", value);
+    }
+
+    let a = 2; 
+    let b = 5;
+    println!("{} + {} = {}", a, b, sum(a,b));
 
 }
